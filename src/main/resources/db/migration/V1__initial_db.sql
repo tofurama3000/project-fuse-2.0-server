a520770a6b45462f6324d99e83dbdefd4ef66b67
Terry <Terry>
2017-11-11 19:09:12 -0700

    add role, findall user, find user by id

--- src/main/resources/db/migration/V1__initial_db.sql
+++ src/main/resources/db/migration/V1__initial_db.sql
@@ -3,13 +3,19 @@ CREATE DATABASE IF NOT EXISTS project_fuse;
 CREATE TABLE IF NOT EXISTS user (
   id INT(11) PRIMARY KEY AUTO_INCREMENT,
   name TEXT,
+  role TEXT,
   encoded_password TEXT,
   email VARCHAR(500)
 );
 
+CREATE TABLE IF NOT EXISTS role (
+  id INT(11) PRIMARY KEY,
+  name TEXT
+);
 
 CREATE TABLE IF NOT EXISTS session (
   session_id varchar(32) PRIMARY KEY,
   created TIMESTAMP,
   email VARCHAR(500)
 );
+
