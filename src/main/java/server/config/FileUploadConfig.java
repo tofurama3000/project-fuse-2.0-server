a1819564aa88763269da0b8d35d5eb1b73954dbf
Qixiang Chao <673545775@qq.com>
2017-11-28 20:33:49 -0700

    add commons-fileupload dependency

--- src/main/java/server/config/FileUploadConfig.java
+++ src/main/java/server/config/FileUploadConfig.java
@@ -22,6 +22,8 @@ public class FileUploadConfig {
         CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
         multipartResolver.setMaxUploadSize(104857600);  //100MB
         multipartResolver.setMaxInMemorySize(1048576); //1MB
+        multipartResolver.setDefaultEncoding("UTF-8");
+        multipartResolver.setResolveLazily(true);
         return multipartResolver;
     }
 }
