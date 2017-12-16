eff681bc154c6e7cfd61c937018f6b8df23911e4
tofurama3000 <tofurama3000@hotmail.com>
2017-12-16 15:40:32 -0700

    Using properties to connect to Elasticsearch host

--- src/main/java/server/utility/ElasticsearchClient.java
+++ src/main/java/server/utility/ElasticsearchClient.java
@@ -11,11 +11,13 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import server.entities.Indexable;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InvalidObjectException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by tofurama on 12/16/17.
@@ -23,19 +25,39 @@ import java.util.List;
 public class ElasticsearchClient {
 
   private ElasticsearchClient() throws UnknownHostException, InvalidObjectException {
+    // Spring is not initialized when this is ran, so we need to get properties ourselves
+    String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    String appConfigPath = rootPath + "application.properties";
+
+    Properties appProps = new Properties();
+    String hostname = "localhost";
+    String tcp_protocol = "http";
+    Integer main_port = 9200;
+    Integer secondary_port = 9201;
+    try {
+      appProps.load(new FileInputStream(appConfigPath));
+      hostname = appProps.getProperty("fuse.elasticsearch_host", hostname);
+      tcp_protocol = appProps.getProperty("fuse.elasticsearch_tcp_protocol", tcp_protocol);
+      main_port = Integer.parseInt(appProps.getProperty("fuse.elasticsearch_port", main_port.toString()));
+      secondary_port = Integer.parseInt(appProps.getProperty("fuse.elasticsearch_sec_port", secondary_port.toString()));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    // Now create the elasticsearch REST client
     this.elasticsearch_client = new RestHighLevelClient(
             RestClient.builder(
-                    new HttpHost("localhost", 9200, "http"),
-                    new HttpHost("localhost", 9201, "http")));
-    if(this.elasticsearch_client == null)
-      throw new InvalidObjectException("Unable to connect to Elasticsearch. Verify Elasticsearch is running!");
+                    new HttpHost(hostname, main_port, tcp_protocol),
+                    new HttpHost(hostname, secondary_port, tcp_protocol)));
   }
 
   public static ElasticsearchClient instance(){
+    // Try to create an instance if one isn't present
     if(inst == null) {
       try {
         inst = new ElasticsearchClient();
       } catch (UnknownHostException | InvalidObjectException e) {
+        // If there was an error, print the stack trace and return null
         e.printStackTrace();
         inst = null;
       }
@@ -43,8 +65,8 @@ public class ElasticsearchClient {
     return inst;
   }
 
+  // Create an index request for an indexable document
   private IndexRequest getIndexRequst(Indexable doc){
-//    return new IndexRequest("test","index_test", "1").source(doc.getEsJson());
     String index = doc.getEsIndex(), type = doc.getEsType(), id = doc.getEsId();
     IndexRequest req;
     if(doc.getEsId() != null) {
@@ -58,7 +80,7 @@ public class ElasticsearchClient {
     return req;
   }
 
-
+  // This is the default async handler, currently it just prints to the console the result
   private static <T> ActionListener<T> getDefaultAsyncHandler(){
     return new ActionListener<T>() {
       @Override
@@ -76,10 +98,12 @@ public class ElasticsearchClient {
     };
   }
 
+  // Perform a synchronous index of a document
   public IndexResponse index(Indexable doc) throws IOException {
     return elasticsearch_client.index(getIndexRequst(doc));
   }
 
+  // Perform an asynchronous index of a document
   public void indexAsync(Indexable doc) {
     elasticsearch_client.indexAsync(getIndexRequst(doc), getDefaultAsyncHandler());
   }
