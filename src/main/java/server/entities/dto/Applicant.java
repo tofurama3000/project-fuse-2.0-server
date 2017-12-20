76f648651a682a113f6611d1f246555fd8c37906
arthad1995 <q714444964@gmail.com>
2017-12-19 21:33:19 -0700

    not sure what to kind of Java type use for time in Applicant entity

--- src/main/java/server/entities/dto/Applicant.java
+++ src/main/java/server/entities/dto/Applicant.java
@@ -1,24 +1,30 @@
-//package server.entities.dto;
-//
-//import lombok.Data;
-//
-//import javax.persistence.*;
-//
-//@Entity
-//@Table(name = "applicant")
-//@Data
-//public class Applicant {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "receiver_id", referencedColumnName = "id")
-//    private User receiver;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "sender_id", referencedColumnName = "id")
-//    private User sender;
-//
-//    private String status;
-//}
+package server.entities.dto;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "team_applicant")
+@Data
+public class Applicant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", referencedColumnName = "id")
+    private User sender;
+
+  @Column(name = "status")
+    private String status;
+
+  @Column(name = "team_id")
+  private long team_id;
+
+  @Column(name = "time")
+  private Timestamp time;
+  
+
+}
