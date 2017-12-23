81c432e6dfe5b8fb5824737011b46dac2d47e58c
tofurama3000 <tofurama3000@hotmail.com>
2017-12-23 15:39:27 -0700

    Added endpoints for specific searches

--- src/main/java/server/controllers/rest/SearchController.java
+++ src/main/java/server/controllers/rest/SearchController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import server.controllers.rest.response.GeneralResponse;
 import server.entities.dto.SearchParams;
+import server.entities.dto.User;
+import server.entities.dto.group.organization.Organization;
+import server.entities.dto.group.project.Project;
+import server.entities.dto.group.team.Team;
 import server.utility.ElasticsearchClient;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,7 +32,7 @@ public class SearchController {
 
   @PostMapping
   @ResponseBody
-  public GeneralResponse searchAll(@RequestBody SearchParams params, HttpServletRequest request, HttpServletResponse response) {
+  public GeneralResponse searchAll(@RequestBody SearchParams params, HttpServletResponse response) {
     List<String> errors = new ArrayList<>();
 
     if(params.getSearchString() == null){
@@ -44,11 +48,56 @@ public class SearchController {
       types = SearchParams.allTypes();
     }
 
+    return new GeneralResponse(
+            response,
+            OK,
+            errors,
+            doSearch(params,
+                    response,
+                    indices.stream().toArray(String[]::new),
+                    types.stream().toArray(String[]::new)
+            )
+    );
+  }
+
+  @PostMapping("/users")
+  @ResponseBody
+  public GeneralResponse searchUser(@RequestBody SearchParams params, HttpServletResponse response) {
+    return doSearch(params, response, new String[]{User.esIndex()}, new String[]{User.esType()});
+  }
+
+  @PostMapping("/projects")
+  @ResponseBody
+  public GeneralResponse searchProjects(@RequestBody SearchParams params, HttpServletResponse response) {
+    return doSearch(params, response, new String[]{Project.esIndex()}, new String[]{Project.esType()});
+  }
+
+  @PostMapping("/teams")
+  @ResponseBody
+  public GeneralResponse searchTeams(@RequestBody SearchParams params, HttpServletResponse response) {
+    return doSearch(params, response, new String[]{Team.esIndex()}, new String[]{Team.esType()});
+  }
+
+  @PostMapping("/organizations")
+  @ResponseBody
+  public GeneralResponse searchOrganizations(@RequestBody SearchParams params, HttpServletResponse response) {
+    return doSearch(params, response, new String[]{Organization.esIndex()}, new String[]{Organization.esType()});
+  }
+
+  private static <T> GeneralResponse doSearch(SearchParams params, HttpServletResponse response,
+                                              String[] indices, String[] types) {
+    List<String> errors = new ArrayList<>();
+
+    if(params.getSearchString() == null) {
+      errors.add("Missing search string");
+      return new GeneralResponse(response, BAD_DATA, errors);
+    }
+
     return new GeneralResponse(response, OK, errors,
             ElasticsearchClient.instance().searchSimpleQuery(
-              indices.stream().toArray(String[]::new),
-              types.stream().toArray(String[]::new),
-              params.getSearchString())
+                    indices,
+                    types,
+                    params.getSearchString())
     );
   }
 }
