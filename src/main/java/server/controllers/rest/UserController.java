24c6f4b7f4fe6ad50191a3cab4f524a939dc102e
cqx820 <673545775dana@gmail.com>
2018-01-18 16:20:18 -0700

    File upload/download endpoints moved

--- src/main/java/server/controllers/rest/UserController.java
+++ src/main/java/server/controllers/rest/UserController.java
@@ -18,15 +18,8 @@ import static server.controllers.rest.response.GeneralResponse.Status.BAD_DATA;
 import static server.controllers.rest.response.GeneralResponse.Status.DENIED;
 import static server.controllers.rest.response.GeneralResponse.Status.OK;
 
-import com.google.common.hash.Hashing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.AlternativeJdkIdGenerator;
 import org.springframework.util.IdGenerator;
@@ -36,9 +29,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import server.controllers.FuseSessionController;
 import server.controllers.MembersOfGroupController;
 import server.controllers.rest.response.GeneralResponse;
@@ -47,7 +38,6 @@ import server.email.StandardEmailSender;
 import server.entities.PossibleError;
 import server.entities.dto.FuseSession;
 import server.entities.dto.UnregisteredUser;
-import server.entities.dto.UploadFile;
 import server.entities.dto.User;
 import server.entities.dto.group.GroupInvitation;
 import server.entities.dto.group.interview.Interview;
@@ -81,12 +71,6 @@ import server.utility.RolesUtility;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -552,75 +536,7 @@ public class UserController {
         return new PossibleError(Status.OK);
     }
 
-    @PostMapping(path = "/fileUpload")
-    @ResponseBody
-    public GeneralResponse fileUpload(@RequestParam("file") MultipartFile fileToUpload, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        List<String> errors = new ArrayList<>();
 
-        Optional<FuseSession> session = fuseSessionController.getSession(request);
-        if (!session.isPresent()) {
-            errors.add(INVALID_SESSION);
-            return new GeneralResponse(response, GeneralResponse.Status.DENIED, errors);
-        }
-        User currentUser = session.get().getUser();
-        UploadFile uploadFile;
-        if (fileToUpload != null) {
-            if (fileToUpload.getSize() > 0 && fileToUpload.getName().equals("file")) {
-                uploadFile = new UploadFile();
-
-                String hash = Hashing.sha256()
-                        .hashString(fileToUpload.getOriginalFilename(), StandardCharsets.UTF_8)
-                        .toString();
-                Timestamp ts = new Timestamp(System.currentTimeMillis());
-                long timestamp = ((ts.getTime())/1000) * 1000;
-                String fileName = hash + "." + timestamp + "." + currentUser.getId().toString();
-                File fileToSave = new File(fileUploadPath, fileName);
-                fileToSave.createNewFile();
-                fileToUpload.transferTo(fileToSave);
-                uploadFile.setHash(hash);
-                uploadFile.setUpload_time(new Timestamp(timestamp));
-                uploadFile.setFile_size(fileToUpload.getSize());
-                uploadFile.setFileName(fileToUpload.getOriginalFilename());
-                uploadFile.setMime_type(fileToUpload.getContentType());
-                uploadFile.setUser(currentUser);
-                return new GeneralResponse(response, OK, null, fileRepository.save(uploadFile));
-            }
-        }
-        errors.add("Invalid file, unable to save");
-        return new GeneralResponse(response, BAD_DATA, errors);
-    }
-
-    @GetMapping(path = "/fileDownload/{id}")
-    @ResponseBody
-    public ResponseEntity<Resource> fileDownload(@PathVariable(value = "id") Long id, HttpServletResponse response, HttpServletRequest request) throws Exception {
-        List<String> errors = new ArrayList<>();
-
-        Optional<FuseSession> session = fuseSessionController.getSession(request);
-        if (!session.isPresent()) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return null;
-        }
-        User currentUser = session.get().getUser();
-        UploadFile fileToFind = fileDownloadRepository.findOne(id);
-        if (fileToFind == null) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return null;
-        }
-        String contentType = fileToFind.getMime_type();
-        String originalFileName = fileToFind.getFileName();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + originalFileName);
-        String fileName = fileToFind.getHash() + "." + fileToFind.getUpload_time().getTime() + "." + fileToFind.getUser().getId();
-        Path path = Paths.get(fileUploadPath, fileName);
-        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
-        File file = new File(fileUploadPath, fileName);
-
-        return ResponseEntity.ok()
-                .headers(headers)
-                .contentLength(file.length())
-                .contentType(MediaType.parseMediaType("application/octet-stream")).body(resource);
-    }
 
     @GetMapping(path = "/{id}")
     @ResponseBody
