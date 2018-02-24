752aa0d53d17819b291d117b5d0510cc9c826c4c
matttolman <tofurama3000@users.noreply.github.com>
2018-02-23 17:51:55 -0700

    Fixed server

--- src/main/resources/db/migration/V23__notification_action.sql
+++ src/main/resources/db/migration/V23__notification_action.sql
@@ -4,13 +4,10 @@ ALTER table notification
   CHANGE COLUMN `object_Type` `data_type` VARCHAR(60);
 
 ALTER table project_invitation
-  ADD COLUMN `applicant_id` INT(11) NOT NULL DEFAULT 0,
-  ADD FOREIGN KEY project_applicant_key (`applicant_id`) REFERENCES `project_applicant`(`id`);
+  ADD COLUMN `applicant_id` INT(11) NOT NULL DEFAULT 0;
 
 ALTER table organization_invitation
-  ADD COLUMN `applicant_id` INT(11) NOT NULL DEFAULT 0,
-  ADD FOREIGN KEY organization_applicant_key (`applicant_id`) REFERENCES `organization_applicant`(`id`);
+  ADD COLUMN `applicant_id` INT(11) NOT NULL DEFAULT 0;
 
 ALTER table team_invitation
-  ADD COLUMN `applicant_id` INT(11) NOT NULL DEFAULT 0,
-  ADD FOREIGN KEY team_applicant_key (`applicant_id`) REFERENCES `team_applicant`(`id`);
+  ADD COLUMN `applicant_id` INT(11) NOT NULL DEFAULT 0;
