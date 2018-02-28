488a6f3a9e86a8296802067f41d9bcad0bd5655f
matttolman <tofurama3000@users.noreply.github.com>
2018-02-27 17:29:40 -0700

    Fixed not null

--- src/main/resources/db/migration/V20__profile_images.sql
+++ src/main/resources/db/migration/V20__profile_images.sql
@@ -1,17 +1,17 @@
 ALTER table project_profile
-  ADD COLUMN thumbnail_id INT(11) NOT NULL DEFAULT 0,
-  ADD COLUMN background_Id INT(11) NOT NULL DEFAULT 0;
+  ADD COLUMN thumbnail_id INT(11),
+  ADD COLUMN background_Id INT(11);
 
 ALTER table team_profile
-  ADD COLUMN thumbnail_id INT(11) NOT NULL DEFAULT 0,
-  ADD COLUMN background_Id INT(11) NOT NULL DEFAULT 0;
+  ADD COLUMN thumbnail_id INT(11),
+  ADD COLUMN background_Id INT(11);
 
 
 ALTER table organization_profile
-  ADD COLUMN thumbnail_id INT(11) NOT NULL DEFAULT 0,
-  ADD COLUMN background_Id INT(11) NOT NULL DEFAULT 0;
+  ADD COLUMN thumbnail_id INT(11),
+  ADD COLUMN background_Id INT(11);
 
 
 ALTER table user_profile
-  ADD COLUMN thumbnail_id INT(11) NOT NULL DEFAULT 0,
-  ADD COLUMN background_Id INT(11) NOT NULL DEFAULT 0;
+  ADD COLUMN thumbnail_id INT(11),
+  ADD COLUMN background_Id INT(11);
